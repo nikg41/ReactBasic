8c7469d508bb6aae381b54846089293caa8a7ee2
Nikhil Garg <nikhil.garg@prudential.com.sg>
2021-11-19 22:41:14 +0530

    Chnaged hardcode to list

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,17 +3,12 @@ import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 const ExpenseItem = (props) => {
-	const [title, setTitle] = useState(props.title)
-	const clickHandler = () => {
-		setTitle("Updated!")
-	}
 	return (
 		<Card className="expense-item">
 			<ExpenseDate date={props.date} />
 			<div className="expense-item__discription">
-				<h2>{title}</h2>
+				<h2>{props.title}</h2>
 				<div className="expense-item__price">${props.amount}</div>
-				<button onClick={clickHandler}>Change Title</button>
 			</div>
 		</Card>
 	);
